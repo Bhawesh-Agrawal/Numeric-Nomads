74177220be91ac30b30d440977177ba3925b6998
Bhawesh Agrawal <bhawesha4@gmail.com>
2025-06-15 09:42:44 +0545

    Update main.py

--- June Hackathon/main.py
+++ June Hackathon/main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI
 from api.joblisting.joblisting import router as jobs_router
 from api.joblisting.fraud_detect import router as fraud_detect_router
 from api.fraud_detection import router as fraud_check_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # for dev use "*" or ["http://localhost:3000"] for production
+    allow_credentials=True,
+    allow_methods=["*"],  # allows GET, POST, OPTIONS, etc.
+    allow_headers=["*"],  # allows all headers including 'Content-Type'
+)
 
 app.include_router(jobs_router, prefix="/jobs", tags=["Jobs"])
 app.include_router(fraud_detect_router, prefix="/jobs/fraud_detect", tags=["Jobs with Fraud Detection"])
